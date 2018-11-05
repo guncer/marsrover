4215096b0be878d7931afeed8cfa330674e1c4cd
guncer <guncer1@gmail.com>
2018-11-05 12:21:54 +0300

    pattern çözümü eklendi

--- marsrover/Program.cs
+++ marsrover/Program.cs
@@ -31,6 +31,12 @@ namespace marsrover
                     instructions1);
                 string two = Calculate(rover2[2], Convert.ToInt32(rover2[0]), Convert.ToInt32(rover2[1]),
                     instructions2);
+                //Chainofresponsiblity solution
+                //string one = CalculateWithPattern(rover1[2], Convert.ToInt32(rover1[0]), Convert.ToInt32(rover1[1]),
+                //    instructions1);
+                //string two = CalculateWithPattern(rover2[2], Convert.ToInt32(rover2[0]), Convert.ToInt32(rover2[1]),
+                //    instructions2);
+
                 Console.WriteLine("Expected Output:");
                 Console.WriteLine(one);
                 Console.WriteLine(two);
@@ -85,6 +91,104 @@ namespace marsrover
             return x.ToString() + y + lastposition;
 
         }
+        static string CalculateWithPattern(string lastposition, int x, int y, string instructions) //chainofresponsibility
+        {
+            Handler h1 = new LeftHandler();
+            Handler h2 = new RightHandler();
+            Handler h3 = new MHandler3();
+            h1.SetNext(h2);
+            h2.SetNext(h3);
+            foreach (char instruction in instructions)
+            {
+                var tupple = h1.HandleRequest(instruction, lastposition, x, y);
+                lastposition = tupple.Item1;
+                x = tupple.Item2;
+                y = tupple.Item3;
 
+            }
+
+            return x.ToString() + y + lastposition;
+
+        }
+
+        abstract class Handler
+        {
+            protected Handler next;
+            public void SetNext(Handler next)
+            {
+                this.next = next;
+            }
+            public abstract Tuple<string, int, int> HandleRequest(char instruction, string position, int x, int y);
+        }
+        class LeftHandler : Handler
+        {
+            public override Tuple<string, int, int> HandleRequest(char instruction, string lastposition, int x, int y)
+            {
+                if (instruction == 'L')
+                {
+                    if (lastposition == "N")
+                        lastposition = "W";
+                    else if (lastposition == "E")
+                        lastposition = "N";
+                    else if (lastposition == "W")
+                        lastposition = "S";
+                    else if (lastposition == "S")
+                        lastposition = "E";
+
+                }
+                else if (next != null)
+                {
+                    return next.HandleRequest(instruction, lastposition, x, y);
+
+                }
+                return new Tuple<string, int, int>(lastposition, x, y);
+
+            }
+        }
+
+        class RightHandler : Handler
+        {
+            public override Tuple<string, int, int> HandleRequest(char instruction, string lastposition, int x, int y)
+            {
+                if (instruction == 'R')
+                {
+                    if (lastposition == "N")
+                        lastposition = "E";
+                    else if (lastposition == "E")
+                        lastposition = "S";
+                    else if (lastposition == "W")
+                        lastposition = "N";
+                    else if (lastposition == "S")
+                        lastposition = "W";
+
+                }
+                else if (next != null)
+                {
+                    return next.HandleRequest(instruction, lastposition, x, y);
+                }
+                return new Tuple<string, int, int>(lastposition, x, y);
+            }
+        }
+        class MHandler3 : Handler
+        {
+            public override Tuple<string, int, int> HandleRequest(char instruction, string lastposition, int x, int y)
+            {
+                if (instruction == 'M')
+                {
+                    if (lastposition == "N")
+                        y++;
+                    else if (lastposition == "S")
+                        y--;
+                    else if (lastposition == "E")
+                        x++;
+                    else if (lastposition == "W")
+                        x--;
+
+                }
+
+                return new Tuple<string, int, int>(lastposition, x, y);
+
+            }
+        }
     }
 }
